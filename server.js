acbdacad1dd12f607cb66e6cc13c15d884b53a0c
Abhijitmalla <abhijitmalla06@gmail.com>
2026-07-20 14:38:24 +0530

    done

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(
       "http://localhost:5501",
       "http://localhost:5000",
       "http://127.0.0.1:5000",
+      "http://127.0.0.1:5500",  
       "https://vkservicesenterprise.in",
       "https://www.vkservicesenterprise.in",
     ],
